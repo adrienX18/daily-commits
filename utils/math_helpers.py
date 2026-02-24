c07e1407942b563d5033a59ea57b6d61f08d6316
Adrien Lemercier <55878471+adrienX18@users.noreply.github.com>
2026-02-24 14:37:42 -0800

    feat: add least common multiple (lcm) function

--- utils/math_helpers.py
+++ utils/math_helpers.py
@@ -33,3 +33,10 @@ def gcd(a: int, b: int) -> int:
     while b:
         a, b = b, a % b
     return abs(a)
+
+
+def lcm(a: int, b: int) -> int:
+    """Calculate the least common multiple of two numbers."""
+    if a == 0 or b == 0:
+        return 0
+    return abs(a * b) // gcd(a, b)
